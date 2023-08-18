449767e7fff6c6ed81c4e4b41bbe51f4fbb1eafd
Taras Halaka <taras.halaka@gmail.com>
2023-08-18 16:45:17 +0300

    0 point

---
+++ README.md
@@ -0,0 +1,9 @@
+# A newsradar Flet app
+
+An example of a minimal Flet app.
+
+To run the app:
+
+```
+flet run [app_directory]
+```
---
+++ main.py
@@ -0,0 +1,8 @@
+from flet import *
+
+
+def main(page: Page):
+    page.add(SafeArea(Text("Hello everybody, from sample Flet!")))
+
+
+app(main)
