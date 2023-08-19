055e249f5da81afb3ba8ac0501536f4fdf9f9165
Taras Halaka <taras.halaka@gmail.com>
2023-08-19 15:56:03 +0300

    looks short, but useless, no on_click() is working

--- main.py
+++ main.py
@@ -1,81 +1,19 @@
 from flet import *
 from src.ui import *
 
-
 def main(page: Page):
     page.title="some app"
-
-
-
-        
-        
-    def left_menu_selection(self):
-        page.controls.clear()
-        print(f" navRail.selected_index = {navRail.selected_index}")
-        if navRail.selected_index == 0:
-            body=Column([ Text("Body as News!")], alignment=MainAxisAlignment.START, expand=True)
-            print(f" News")
-        elif navRail.selected_index == 1:
-            body=Column([ Text("check lists!")], alignment=MainAxisAlignment.START, expand=True)
-            print(f" CheckList")
-        elif navRail.selected_index == 2:
-            body=Column([ Text("settings!")], alignment=MainAxisAlignment.START, expand=True) 
-            print(f"Settings")     
-        
-        page.add(
-            Row(
-                [
-                    navRail,
-                    VerticalDivider(width=1),
-                    body,
-                    UI()       
-                ],
-                expand=True,
-            )
-        )             
-        page.update()
-        print("menu item seelction updated")
-    
-    navRail=NavigationRail(
-        selected_index=0,
-        label_type=NavigationRailLabelType.ALL,
-        # extended=True,
-        min_width=100,
-        min_extended_width=400,
-        group_alignment=-0.9,
-        destinations=[
-            NavigationRailDestination(
-                icon=icons.NEWSPAPER, selected_icon=icons.NEWSPAPER, label="News"
-            ),
-            NavigationRailDestination(
-                icon_content=Icon(icons.FACT_CHECK_OUTLINED),
-                selected_icon_content=Icon(icons.FACT_CHECK),
-                label="CheckLists",
-            ),
-            NavigationRailDestination(
-                icon=icons.SETTINGS_OUTLINED,
-                selected_icon_content=Icon(icons.SETTINGS),
-                label_content=Text("Settings"),
-            ),
-        ],
-        #on_change=lambda e: print("Selected destination:", e.control.selected_index),
-        on_change=left_menu_selection
-    )
-
-        
-    
-    body=Column([ Text("Empty")], alignment=MainAxisAlignment.START, expand=True)
-        
-    #page.appbar=AppBarMain(page)
-    # appBarMain=AppBarMain()
-    # print("\n \n",appBarMain,"\n")
-    #page.appbar=AppBar3()
-    page.appbar=appBar2
+    page.appbar=staticAppBar
+    page.add(
+        Row(
+            [
+                staticNavRail,
+                VerticalDivider(width=1),
+                UI()       
+            ],
+            expand=True,
+        )
+    )             
     page.update()
-    left_menu_selection(page)
-    
-
-    
-
-#app(main, view=AppView.WEB_BROWSER)
+    left_menu_selection()
 app(main, view=AppView.FLET_APP)
--- src/ui.py
+++ src/ui.py
@@ -3,16 +3,22 @@ from flet import *
 class UI(UserControl):
     def __init__(self):
         super().__init__()
+        self.counter=0
+        
+    def add_click(self, e):
+        self.counter += 1
+        self.update()
 
 
     def build(self):
         return Column(
-             [ElevatedButton("Add1", on_click=lambda e : print(e.control.text,"some")),
-              ElevatedButton("Add2", on_click=lambda _:print("Add2")) ,
-              ElevatedButton("Add3", on_click=lambda _:print("Add3"))]
+             [
+                ElevatedButton("Add1", on_click=self.add_click),
+                Text(f" clicked {str(self.counter)}") 
+             ]
         )
 
-appBar2=AppBar(
+staticAppBar=AppBar(
     leading=FloatingActionButton(icon=icons.MENU_SHARP, on_click=lambda e: print("page.controls")),
     leading_width=40,
     title=Text("Some app bar"),
@@ -33,63 +39,42 @@ appBar2=AppBar(
                 ]
 )
 
-def hide_left_navigation(page:Page):
-    # body.controls.append(Text("menu item was hidden"))
-    # body.update()
-    # print("Left navigation collapsed")
-    page.navRail.visible = not page.navRail.visible
-    page.update()
+def left_menu_selection():
+    selected=staticNavRail.selected_index
+    print(f" navRail.selected_index = {selected}")
+    if selected == 0:
+        body=Column([ Text("Body as News!")], alignment=MainAxisAlignment.START, expand=True)
+        print(f" News")
+    elif selected == 1:
+        body=Column([ Text("check lists!")], alignment=MainAxisAlignment.START, expand=True)
+        print(f" CheckList")
+    elif selected == 2:
+        body=Column([ Text("settings!")], alignment=MainAxisAlignment.START, expand=True) 
+        print(f"Settings")     
+    
 
-
-class AppBar3(UserControl):
-    def __init_(self):
-        super().__init__()
-        
-    def build(self):
-        return AppBar(
-        leading=FloatingActionButton(icon=icons.MENU_SHARP, on_click=self.hide_left_navigation),
-        leading_width=40,
-        title=Text("Some app bar"),
-        toolbar_height=40,
-        center_title=False,
-        bgcolor=colors.BLUE_300,
-        actions=[
-                    PopupMenuButton(
-                        icon=icons.SETTINGS,
-                        items=[
-                            PopupMenuItem(text="Help"),
-                            PopupMenuItem(),  # divider
-                            PopupMenuItem(text="Settings"),
-                            PopupMenuItem(text="About"),
-                            PopupMenuItem(text="Hide NavBar", on_click=self.hide_left_navigation )
-                        ]
-                    ),
-                ])                
-        
-    def hide_left_navigation(self):
-        # body.controls.append(Text("menu item was hidden"))
-        # body.update()
-        # print("Left navigation collapsed")
-        self.page.navRail.visible = not self.page.navRail.visible
-        self.page.update()
-            
-    # def build(self):
-    #     return AppBar(leading=FloatingActionButton(icon=icons.MENU_SHARP, on_click=self.hide_left_navigation),
-    #                     leading_width=40,
-    #                     title=Text("Some app bar"),
-    #                     toolbar_height=40,
-    #                     center_title=False,
-    #                     bgcolor=colors.BLUE_300,
-    #                     actions=[
-    #                         PopupMenuButton(
-    #                             icon=icons.SETTINGS,
-    #                             items=[
-    #                                 PopupMenuItem(text="Help"),
-    #                                 PopupMenuItem(),  # divider
-    #                                 PopupMenuItem(text="Settings"),
-    #                                 PopupMenuItem(text="About"),
-    #                                 PopupMenuItem(text="Hide NavBar", on_click=self.hide_left_navigation )
-    #                             ]
-    #                         ),
-    #                     ]
-    #                 )
+staticNavRail=NavigationRail(
+        selected_index=0,
+        label_type=NavigationRailLabelType.ALL,
+        # extended=True,
+        min_width=100,
+        min_extended_width=400,
+        group_alignment=-0.9,
+        destinations=[
+            NavigationRailDestination(
+                icon=icons.NEWSPAPER, selected_icon=icons.NEWSPAPER, label="News"
+            ),
+            NavigationRailDestination(
+                icon_content=Icon(icons.FACT_CHECK_OUTLINED),
+                selected_icon_content=Icon(icons.FACT_CHECK),
+                label="CheckLists",
+            ),
+            NavigationRailDestination(
+                icon=icons.SETTINGS_OUTLINED,
+                selected_icon_content=Icon(icons.SETTINGS),
+                label_content=Text("Settings"),
+            ),
+        ],
+        #on_change=lambda e: print("Selected destination:", e.control.selected_index),
+        on_change=left_menu_selection
+    )
