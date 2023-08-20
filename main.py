1ba7871141599c2dda7c2ba788451364d3fb06ad
Taras Halaka <taras.halaka@gmail.com>
2023-08-20 07:56:38 +0300

    shows UI clases without UserControl

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flet import *
-from src.ui import *
+from src.ui2 import *
 
 def main(page: Page):
-    page.title="some app"
+    
     CustomApp(page)
-    page.update()
+    
     
 app(main, view=AppView.FLET_APP)
