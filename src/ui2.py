bd0aa3529e7153de015fdec177e14599cc4a4210
Taras Halaka <taras.halaka@gmail.com>
2023-08-20 08:47:33 +0300

    SideBar added no UserControl was defined, but no active actions yet

--- src/ui2.py
+++ src/ui2.py
@@ -7,15 +7,9 @@ class CustomApp():
         self.page.title="some sort of App"
 
         self.page.appbar=mAppBar().appBar
-        self.page.add(
-                        Column([
-                            TextField(label="Your name"),
-                            ElevatedButton("Login")
-                        ])
-                    )        
+        mNavRail(self.page)
+        # self.page.add(Row(mNavRail(self.page)))        
         self.page.update()
-
-
 
 class mAppBar():
     def __init__(self):
@@ -40,3 +34,62 @@ class mAppBar():
                     ),
                 ]
 )
+
+
+
+class mNavRail():
+    def __init__(self,source_page):
+        super().__init__()
+        self.page=source_page
+        self.navRail=NavigationRail(
+                                selected_index=0,
+                                label_type=NavigationRailLabelType.ALL,
+                                height=1024,
+                                extended=False,
+
+                                min_width=100,
+                                min_extended_width=400,
+                                group_alignment=-0.9,
+                                destinations=[
+                                    NavigationRailDestination(
+                                        icon=icons.NEWSPAPER, selected_icon=icons.NEWSPAPER, label="News"
+                                    ),
+                                    NavigationRailDestination(
+                                        icon_content=Icon(icons.FACT_CHECK_OUTLINED),
+                                        selected_icon_content=Icon(icons.FACT_CHECK),
+                                        label="CheckLists",
+                                    ),
+                                    NavigationRailDestination(
+                                        icon=icons.SETTINGS_OUTLINED,
+                                        selected_icon_content=Icon(icons.SETTINGS),
+                                        label_content=Text("Settings"),
+                                    ),
+                                ],
+                                #on_change=lambda e: print("Selected destination:", e.control.selected_index),
+                                #on_change=customApp.left_menu_selection
+                                on_change=self.left_menu_selection
+                            )
+        self.page.add(Row(controls=[self.navRail,
+                            VerticalDivider(width=1),
+                            Column([
+                                    TextField(label="Your name"),
+                                    ElevatedButton("Login"),
+                                        ],
+                                   )
+                            ]
+                          ))
+    
+    def left_menu_selection(self):
+        print("left_menu_selection  - is called ")
+        selected=self.navRail.selected_index
+        print(f" navRail.selected_index = {selected}")
+        if selected == 0:
+            body=Column([ Text("Body as News!")], alignment=MainAxisAlignment.START, expand=True)
+            print(f" News")
+        elif selected == 1:
+            body=Column([ Text("check lists!")], alignment=MainAxisAlignment.START, expand=True)
+            print(f" CheckList")
+        elif selected == 2:
+            body=Column([ Text("settings!")], alignment=MainAxisAlignment.START, expand=True) 
+            print(f"Settings")
+        self.update()
